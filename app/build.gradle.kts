fc914051fbba5aa6f85b4496643bc59626d17ca6
Matheus Fábio <matheusfabiors@gmail.com>
2024-05-16 21:28:32 -0300

    backup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 
 android {
     namespace = "br.com.example.aboutme"
+
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
     compileSdk = 34
 
     defaultConfig {
@@ -42,6 +48,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.databinding:databinding-runtime:8.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
